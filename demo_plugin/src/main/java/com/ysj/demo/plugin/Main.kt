ce58aad305379de6f926e0b520759795d639bb6e
Ysj001 <543501451@qq.com>
2021-08-26 18:11:19 +0800

    <新增> 演示挂载 Modifier 的 demo plugin

---
+++ demo_plugin/src/main/java/com/ysj/demo/plugin/Main.kt
@@ -0,0 +1,18 @@
+package com.ysj.demo.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ *
+ *
+ * @author Ysj
+ * Create time: 2021/8/25
+ */
+class Main : Plugin<Project> {
+
+    override fun apply(target: Project) {
+
+    }
+
+}
---
+++ demo_plugin/src/main/java/com/ysj/demo/plugin/TestModifier.kt
@@ -0,0 +1,25 @@
+package com.ysj.demo.plugin
+
+import com.android.build.api.transform.Transform
+import com.ysj.lib.bytecodeutil.modifier.IModifier
+import org.objectweb.asm.tree.ClassNode
+
+/**
+ *
+ *
+ * @author Ysj
+ * Create time: 2021/8/25
+ */
+class TestModifier(
+    override val transform: Transform,
+    override val allClassNode: Map<String, ClassNode>,
+) : IModifier {
+
+    override fun modify(classNode: ClassNode) {
+    }
+
+    override fun scan(classNode: ClassNode) {
+        println("demo-plugin --> ${classNode.name}")
+
+    }
+}
