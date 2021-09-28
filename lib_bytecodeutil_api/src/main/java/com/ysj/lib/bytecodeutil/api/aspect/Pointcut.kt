664f14289816958ab111d4d6fd5c05d6b7169ab3
Ysj001 <543501451@qq.com>
2021-09-28 12:09:37 +0800

    <完善> 注释和默认参数

--- lib_bytecodeutil_api/src/main/java/com/ysj/lib/bytecodeutil/api/aspect/Pointcut.kt
+++ lib_bytecodeutil_api/src/main/java/com/ysj/lib/bytecodeutil/api/aspect/Pointcut.kt
@@ -13,27 +13,15 @@ import java.util.regex.Pattern
 @Retention(AnnotationRetention.BINARY)
 annotation class Pointcut(
     /**
-     * 目标切入点
+     * 目标切入点，采用正则表达式 [Pattern.matches] 来匹配
      *
      * 不同的前缀表达不同的含义：
-     * - "class:" --> 类
-     * - "superClass:" --> 父类
-     * - "interface:" --> 接口
-     * - "annotation:" --> 注解
+     * - "class:" --> 匹配类全限定名
+     * - "superClass:" --> 匹配类继承的父类全限定名
+     * - "interface:" --> 匹配类所继承的接口全限定名
+     * - "annotation:" --> 匹配方法上的注解的描述。使用该前缀不需要写 [funName] 和 [funDesc]
      */
     val target: String,
-    /**
-     * 切入点方法名，用于确定切入的方法
-     *
-     * 采用正则表达式 [Pattern.matches] 来匹配
-     */
-    val funName: String,
-    /**
-     * 切入点方法描述，用于确定切入的具体方法
-     *
-     * 采用正则表达式 [Pattern.matches] 来匹配
-     */
-    val funDesc: String,
     /**
      * 切入点的具体执行位置
      * - see: [POSITION_START]
@@ -41,6 +29,18 @@ annotation class Pointcut(
      * - see: [POSITION_CALL]
      */
     val position: Int,
+    /**
+     * 切入点方法名，用于确定切入的方法
+     *
+     * 采用正则表达式 [Pattern.matches] 来匹配
+     */
+    val funName: String = ".*.",
+    /**
+     * 切入点方法描述，用于确定切入的具体方法
+     *
+     * 采用正则表达式 [Pattern.matches] 来匹配
+     */
+    val funDesc: String = ".*.",
 )
 
 /** 插入方法开头 */
