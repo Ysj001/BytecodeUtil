a60264a06210d941195a4b6b111c4949d1fdcf65
Ysj <543501451@qq.com>
2023-10-15 17:12:36 +0800

    feat：更新 demo

--- app/src/main/java/com/ysj/demo/MainActivity.kt
+++ app/src/main/java/com/ysj/demo/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.ysj.demo
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ysj.demo.databinding.ActivityMainBinding
-import com.ysj.demo.aspect.MainActivity as AspectActivity
-import com.ysj.demo.component.MainActivity as ComponentDiActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +11,8 @@ class MainActivity : AppCompatActivity() {
         val vb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(vb.root)
         vb.toAspectDemo.setOnClickListener {
-            startActivity(Intent(this, AspectActivity::class.java))
         }
         vb.toComponentDiDemo.setOnClickListener {
-            startActivity(Intent(this, ComponentDiActivity::class.java))
         }
     }
 
